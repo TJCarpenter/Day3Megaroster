f04c109f80b2e819987747435f0f6f5bd8db4fa1
Tyler Carpenter <tjcarpenter15@my.trine.edu>
2017-05-17 11:33:56 -0400

    Update files

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
 $(document).foundation()
+
+const megaroster = {
+    students: [],
+
+    init(listSelector) {        
+        this.studentList = document.querySelector(listSelector)
+        this.max = 0
+        document
+            .querySelector('#new-student')
+            .addEventListener('submit', this.addStudent.bind(this))
+    },
+
+    addStudent(ev) {
+        ev.preventDefault()
+        const form = ev.target
+        const student = {
+            id: this.max + 1,
+            name: form.studentName.value,
+        }
+        this.students.unshift(student)
+        const listItem = this.buildListItem(student)
+        this.prependChild(this.studentList, listItem)
+        this.max++
+        form.reset()
+    },
+
+    prependChild(parent, child){
+        parent.insertBefore(child, parent.firstChild)
+    },
+
+    buildListItem(student) {
+        const li = document.createElement('li')
+        li.textContent = student.name
+        li.dataset.id = student.id
+        return li
+    },
+}
+megaroster.init('#studentList')
