7f2b7d1f1680d0cb40ba1d1afa651b94d6d8acdd
Tyler Carpenter <tjcarpenter15@my.trine.edu>
2017-05-17 13:57:35 -0400

    Add promote button

--- js/app.js
+++ js/app.js
@@ -1,45 +1,70 @@
 $(document).foundation()
 
 const megaroster = {
-    students: [],
+  students: [],
 
-    init(listSelector) {        
-        this.studentList = document.querySelector(listSelector)
-        this.max = 0
-        document
-            .querySelector('#new-student')
-            .addEventListener('submit', this.addStudent.bind(this))
-    },
+  init(listSelector) {
+    this.studentList = document.querySelector(listSelector)
+    this.max = 0
+    this.setupEventListeners()  
+  },
 
-    addStudent(ev) {
-        ev.preventDefault()
-        const form = ev.target
-        const student = {
-            id: this.max + 1,
-            name: form.studentName.value,
-        }
-        this.students.unshift(student)
-        const listItem = this.buildListItem(student)
-        this.prependChild(this.studentList, listItem)
-        this.max++
-        form.reset()
-    },
+  setupEventListeners() {
+    document
+      .querySelector('#new-student')
+      .addEventListener('submit', this.addStudent.bind(this))
+  },
 
-    prependChild(parent, child){
-        parent.insertBefore(child, parent.firstChild)
-    },
+  promoteStudent(ev){
+    const btn = ev.target
+    btn.closest('.student').style.color = 'red'
+  },
 
-    buildListItem(student) {
-        const template = document.querySelector('.student.template')
-        const li = template.cloneNode(true)
-        this.removeClassName(li, 'template')
-        li.querySelector('.student-name').textContent = student.name
-        li.dataset.id = student.id
-        return li
-    },
+  removeStudent(ev) {
+    const btn = ev.target
+    btn.closest('.student').remove()
 
-    removeClassName(el, className){
-        el.className = el.className.replace(className, '').trim()
-    },
+    // Remove it from the this.students array
+    // this.students.splice(?, 1)
+  },
+
+  addStudent(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const student = {
+      id: this.max + 1,
+      name: f.studentName.value,
+    }
+    this.students.unshift(student)
+
+    const listItem = this.buildListItem(student)
+    this.prependChild(this.studentList, listItem)
+
+    this.max ++
+    f.reset()
+  },
+
+  prependChild(parent, child) {
+    parent.insertBefore(child, parent.firstChild)
+  },
+
+  buildListItem(student) {
+    const template = document.querySelector('.student.template')
+    const li = template.cloneNode(true)
+    this.removeClassName(li, 'template')
+    li.querySelector('.student-name').textContent = student.name
+    li.dataset.id = student.id
+    li
+      .querySelector('button.remove')
+      .addEventListener('click', this.removeStudent.bind(this))
+    li
+      .querySelector('button.promote')
+      .addEventListener('click', this.promoteStudent.bind(this))
+    return li
+  },
+
+  removeClassName(el, className){
+    el.className = el.className.replace(className, '').trim()
+  }
 }
 megaroster.init('#studentList')
