5c12665e6ea60bfab89cfdb834fdd91c9bc07817
Tyler Carpenter <tjcarpenter15@my.trine.edu>
2017-05-18 11:38:40 -0400

    Convert Megaroster to a class

--- js/app.js
+++ js/app.js
@@ -1,52 +1,89 @@
 $(document).foundation()
 
-const megaroster = {
-  students: [],
-
-  init(listSelector) {
+class Megaroster {
+  constructor(listSelector) {
     this.studentList = document.querySelector(listSelector)
     this.max = 0
-    this.setupEventListeners()  
-  },
+    this.students = []
+    this.setupEventListeners()
+    this.load()
+  }
 
   setupEventListeners() {
     document
       .querySelector('#new-student')
-      .addEventListener('submit', this.addStudent.bind(this))
-  },
+      .addEventListener('submit', this.addStudentViaForm.bind(this))
+  }
 
-  promoteStudent(ev){
-    const btn = ev.target
-    btn.closest('.student').style.color = 'red'
-  },
+  save() {
+    localStorage.setItem('roster', JSON.stringify(this.students))
+  }
+
+  load() {
+    const rosterString = localStorage.getItem('roster')
+    if (rosterString) {
+      const rosterArray = JSON.parse(rosterString)
+      rosterArray
+        .reverse()
+        .map(this.addStudent.bind(this))
+    }
+  }
 
   removeStudent(ev) {
     const btn = ev.target
-    btn.closest('.student').remove()
+    const li = btn.closest('.student')
 
-    // Remove it from the this.students array
-    // this.students.splice(?, 1)
-  },
+    for (let i=0; i < this.students.length; i++) {
+      let currentId = this.students[i].id.toString()
+      if (currentId === li.dataset.id) {
+        this.students.splice(i, 1)
+        break
+      }
+    }
 
-  addStudent(ev) {
+    li.remove()
+    this.save()
+  }
+
+  promoteStudent(student, ev){
+    const btn = ev.target
+    const li = btn.closest('.student')
+    student.promoted = !student.promoted
+
+    if (student.promoted){
+      li.classList.add('promoted')
+    } else {
+      li.classList.remove('promoted')
+    }
+    this.save()
+  }
+
+  addStudentViaForm(ev) {
     ev.preventDefault()
     const f = ev.target
     const student = {
       id: this.max + 1,
       name: f.studentName.value,
     }
+    this.addStudent(student)
+    f.reset()
+  }
+
+  addStudent(student, append) {
     this.students.unshift(student)
 
     const listItem = this.buildListItem(student)
     this.prependChild(this.studentList, listItem)
 
-    this.max ++
-    f.reset()
-  },
+    if (student.id > this.max) {
+      this.max = student.id
+    }
+    this.save()
+  }
 
   prependChild(parent, child) {
     parent.insertBefore(child, parent.firstChild)
-  },
+  }
 
   buildListItem(student) {
     const template = document.querySelector('.student.template')
@@ -54,17 +91,22 @@ const megaroster = {
     this.removeClassName(li, 'template')
     li.querySelector('.student-name').textContent = student.name
     li.dataset.id = student.id
+    if (student.promoted){
+      li.classList.add('promoted')
+    }
+
     li
       .querySelector('button.remove')
       .addEventListener('click', this.removeStudent.bind(this))
     li
       .querySelector('button.promote')
-      .addEventListener('click', this.promoteStudent.bind(this))
+      .addEventListener('click', this.promoteStudent.bind(this, student))
     return li
-  },
+  }
 
   removeClassName(el, className){
     el.className = el.className.replace(className, '').trim()
   }
 }
-megaroster.init('#studentList')
+const roster = new Megaroster('#studentList')
+
